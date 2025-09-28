69cd3c10bc11848167421943e121bdad392aa601
Kelvin Ripardo <nivlik.ripardo@gmail.com>
2025-09-27 23:27:35 -0300

    Ajuste de ortografia no H1 do HTML | Ajuste no fluxo de logica do js(seconds)

--- src/index.js
+++ src/index.js
@@ -4,25 +4,27 @@
       clearInterval(timer);
       const minutes = parseInt(document.getElementById('minutes').value);
       let seconds = minutes * 60;
+
+      if (isNaN(seconds) || seconds <= 0) {
+        alert('Digite o tempo de concentração - 力');
+        document.getElementById('minutes').focus();
+        document.getElementById('minutes').value = '0';
+        return;
+      }
+
       const countdown = document.getElementById('countdown');
       const alertSound = document.getElementById('alertSound');
-
       timer = setInterval(() => {
         const min = Math.floor(seconds / 60 );
         const sec = seconds % 60;
         countdown.textContent = `${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`;
         seconds--;
 
-        if (seconds === 0) {
+        if (seconds < 0) {
           clearInterval(timer);
           countdown.textContent = 'Pausa pro café ☕';
           alertSound.play();
-        }else{
-            alert('Digite o tempo de concentração - 力');
-            document.getElementById('minutes').focus();
-            resetTimer();
-            
-        }
+        } 
       }, 1000);
     }
 
